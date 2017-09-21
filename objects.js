07e1a5e626a4b7c895f496ed5c6af4012a843418
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-21 22:22:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign(playlist, { [artistName]: songTitle })
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.[artist]
+  return playlist
+}
