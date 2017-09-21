61bcd05ff03de4f1288505f82ed0a77a5429a9fd
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-21 22:19:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
 var playlist = { Wilco: 'Impossible Germany'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign(playlist, {[artistName]: [songTitle]})
+  return playlist
+}
